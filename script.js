66cde18cc8bdaa4defc0ff568fa998bbd8ec7577
kellykelly7 <kellytruong519@gmail.com>
2025-02-12 15:22:35 -0500

    added comments

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+// calls for the access tokens from mapbox unique to each user
 mapboxgl.accessToken = 'pk.eyJ1Ijoia2VsbHlrZWxseTciLCJhIjoiY202aWNjdDE5MDcwbTJrcHppYWw5ZjJzcCJ9.pry2p-gu8qXteiF0TWa4dw'; // Add default public map token from your Mapbox account
 
+// new variable for the map that we are importing from mapbox
 const map = new mapboxgl.Map({
     container: 'campus-buildings', // map container ID
     style: 'mapbox://styles/kellykelly7/cm72971jc006x01s33axk653l', // style URL
@@ -7,28 +9,39 @@ const map = new mapboxgl.Map({
     zoom: 14.5, // starting zoom level
 });
 
+//loads data and layers on top of the style basemap
 map.on('load', () => {
     map.addSource('campus-buildings', {
+        //data source type
         type: 'geojson',
-        data: 'https://raw.githubusercontent.com/kellykelly7/ggr472-lab2/refs/heads/main/campusbuildings.geojson' // Your URL to your buildings.geojson file
+        //link to data 
+        data: 'https://raw.githubusercontent.com/kellykelly7/ggr472-lab2/refs/heads/main/campusbuildings.geojson' 
         })
+        // making the polygons added from data source visible with styling
         map.addLayer({
+            // item id
             'id': 'campus-buildings',
+            // type of style -- could be 3D protrusion, circle, line, etc.
             'type': 'fill',
+            // original data source
             'source': 'campus-buildings', 
+            //changes the characteristics of the element
             'layout': {},
+            // changes colour and opacity and other colour-related variables
             'paint': {
                 'fill-color': '#0080ff', 
                 'fill-opacity': 0.8
             }
         });
         map.addLayer({
+            // creates and outline around the polygons 
             'id': 'outline',
             'type': 'line',
             'source': 'campus-buildings',
             'layout': {},
             'paint': {
                 'line-color': '#000',
+                // sets line width by px
                 'line-width': 1
             }
     });
@@ -38,8 +51,10 @@ map.on('load', () => {
         })
         map.addLayer({
             'id': 'campus-path',
+            // making the linestring data from the geojson visible
             'type': 'line',
             'source': 'campus-path',
+            // edits characteristics of layered styling of the data source
             'layout': {
                 'line-join': 'round',
                 'line-cap': 'square'
@@ -50,7 +65,6 @@ map.on('load', () => {
             }
     });
 });
-
 
 // establishing constant variables for all my buttons from the html page with IDs
 const btn = document.getElementById("sidsmith_commons");
